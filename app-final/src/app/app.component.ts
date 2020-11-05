6a4001bccf0f47d95eb17f702982e17eff97bc2b
Charles Ferreira <charles.dev@live.com>
2020-11-05 16:57:14 -0300

    Inclui dados hard-coded e apresenta dinamicamente

--- app-final/src/app/app.component.ts
+++ app-final/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+import { ListaDeTarefas } from './model/lista-de-tarefas';
+
+const LISTAS_DE_TAREFAS: ListaDeTarefas[] = [
+  {
+    nome: 'Tarefas da casa',
+    tarefas: [
+      { nome: 'Varrer a casa', concluída: false },
+      { nome: 'Lavar a louça', concluída: true },
+    ],
+  },
+  {
+    nome: 'Lista de mercado',
+    tarefas: [
+      { nome: 'Feijão', concluída: false },
+      { nome: 'Paçoca', concluída: false },
+    ],
+  },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  listasDeTarefas = LISTAS_DE_TAREFAS;
 }
