e99c3679948007cb0e2031ceecf2c10e8cb1cdc6
Charles Ferreira <charles.dev@live.com>
2020-11-12 23:54:23 -0300

    Remove dados hard-coded

--- app-final/src/app/app.component.ts
+++ app-final/src/app/app.component.ts
@@ -2,30 +2,13 @@ import { Component } from '@angular/core';
 
 import { ListaDeTarefas } from './model/lista-de-tarefas';
 
-const LISTAS_DE_TAREFAS: ListaDeTarefas[] = [
-  {
-    nome: 'Tarefas da casa',
-    tarefas: [
-      { nome: 'Varrer a casa', concluida: false },
-      { nome: 'Lavar a louça', concluida: true },
-    ],
-  },
-  {
-    nome: 'Lista de mercado',
-    tarefas: [
-      { nome: 'Feijão', concluida: false },
-      { nome: 'Paçoca', concluida: false },
-    ],
-  },
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  listasDeTarefas = LISTAS_DE_TAREFAS;
+  listasDeTarefas: ListaDeTarefas[] = [];
   modoEdicaoAtivo = false;
 
   onCriarLista() {
